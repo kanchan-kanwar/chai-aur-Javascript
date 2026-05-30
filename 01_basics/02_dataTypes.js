bef7518e6e1952da178d61958a32b081845815e3
kanchan-kanwar <kanchankanwarmtd04@gmail.com>
2026-05-30 11:41:36 +0000

    dataTypes study

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict" //treat all js code as newer version
+
+// alert("hello") // we are using node js not browser
+
+let name = "kanchan"
+let age = 18
+let isLoggedIn = false
+
+//number -> 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => value not assigned till now
+//symbol => unique
+
+//object
+
+console.log(typeof "kanchan") //string
+console.log(typeof age) //number
+console.log(typeof null) // object
+console.log(typeof undefined) //undefined
